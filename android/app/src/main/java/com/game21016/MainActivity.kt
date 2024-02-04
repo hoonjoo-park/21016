4253b4a15060c041c6f123afa1b3d8601571f814
hoonjoo94 <hoonjoo94@naver.com>
2024-02-04 21:54:19 +0900

    chore: add reanimated, react-navigation, recoil

--- android/app/src/main/java/com/game21016/MainActivity.kt
+++ android/app/src/main/java/com/game21016/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.game21016
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -12,6 +13,10 @@ class MainActivity : ReactActivity() {
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "game21016"
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import {View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 
 function App(): React.JSX.Element {
-  return <View />;
+  return (
+    <NavigationContainer>
+      <View />;
+    </NavigationContainer>
+  );
 }
 
 export default App;
